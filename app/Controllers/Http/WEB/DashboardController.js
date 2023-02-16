2eaab92eaf0932ec812fbe13d7d36d94ad0c4176
voitaraujo <voitilaaraujo@gmail.com>
2023-02-16 10:23:28 -0300

    update

--- app/Controllers/Http/WEB/DashboardController.js
+++ app/Controllers/Http/WEB/DashboardController.js
@@ -19,11 +19,79 @@ class DashboardController {
       const news = await Database.raw(queryNews, [verified.grpven]);
 
       const duplicatas = await Database.raw(queryDuplicatas, [verified.grpven]);
-      const performance = await Database.raw(QueryTelemetrias, [verified.grpven]);
+      const performance = await Database.raw(queryTelemetrias, [
+        verified.grpven,
+      ]);
+
+      const entregaDeOs = await Database.raw(queryEntregaDeOS, [
+        verified.grpven,
+      ]);
+      const faturamentoClientes = await Database.raw(
+        queryFaturamentoDosClientes,
+        [verified.grpven]
+      );
+
       const datas = [];
+
+      // datas.push({
+      //   date: moment().startOf("month").toDate(),
+      //   type: "InvFranq",
+      // });
+      datas.push({
+        date: moment()
+          .subtract(1, "month")
+          .endOf("month")
+          .subtract(1, "day")
+          .subtract(3, "h")
+          .toDate(),
+        type: "InvPilao",
+      });
+      datas.push({
+        date: moment()
+          .subtract(1, "month")
+          .endOf("month")
+          .subtract(3, "h")
+          .toDate(),
+        type: "InvPilao",
+      });
+      datas.push({
+        date: moment().startOf("month").toDate(),
+        type: "InvPilao",
+      });
+      datas.push({
+        date: moment().startOf("month").add(1, "day").toDate(),
+        type: "InvPilao",
+      });
+
+      duplicatas.forEach((d) => {
+        datas.push({ date: d.DtVenc, type: "compra" });
+      });
+
+      entregaDeOs.forEach((eo) => {
+        datas.push({ date: eo.OSCExpDtPrevisao, type: "OS" });
+      });
+
+      faturamentoClientes.forEach((fc) => {
+        const ultimoDiaDoMes = moment().endOf("month").get("date");
+
+        if (ultimoDiaDoMes < fc.AnxDiaFecha) {
+          datas.push({
+            date: moment().endOf("month").subtract(3, "hours").toDate(),
+            type: "Cliente",
+          });
+        } else {
+          datas.push({
+            date: moment().date(fc.AnxDiaFecha).toDate(),
+            type: "Cliente",
+          });
+        }
+      });
 
       response.status(200).send({
         News: news,
+        Titulos: duplicatas,
+        Performance: [...performance.slice(0, 10)],
+        Datas: datas,
       });
     } catch (err) {
       response.status(400).send();
@@ -137,6 +205,13 @@ const queryNews =
   "select NS.*, NSC.DtConfirmacao from dbo.NewsSLWEB as NS left join dbo.NewsSLWEBConfirmacao as NSC on NS.NewsId = NSC.NewsId and NSC.GrpVen = ? where NS.BannerStatus = 'A' order by NS.NewsId DESC";
 
 const queryDuplicatas =
-  "SELECT * FROM ( SE1_GrpVen INNER JOIN SE1_Class ON (SE1_GrpVen.E1_PREFIXO = SE1_Class.E1_PREFIXO) AND (SE1_GrpVen.E1_TIPO = SE1_Class.E1_TIPO) ) LEFT JOIN dbo.SE1DtVenc ON SE1_GrpVen.DtVenc = dbo.SE1DtVenc.SE1DtVenc WHERE SE1_GrpVen.GrpVen = ?";
+  "SELECT E1_NUM, E1Desc, DtVenc, E1_SALDO FROM ( SE1_GrpVen INNER JOIN SE1_Class ON (SE1_GrpVen.E1_PREFIXO = SE1_Class.E1_PREFIXO) AND (SE1_GrpVen.E1_TIPO = SE1_Class.E1_TIPO) ) LEFT JOIN dbo.SE1DtVenc ON SE1_GrpVen.DtVenc = dbo.SE1DtVenc.SE1DtVenc WHERE SE1_GrpVen.GrpVen = ?";
 
-  const QueryTelemetrias = "SELECT bog.GrpVen, bog.EquiCod, P.AnxDesc, bog.MáxDeDataLeitura, bog.[Ql-4], bog.[Ql-3], bog.[Ql-2], bog.[Ql-1], bog.Ql0, bog.[Con-4], bog.[Con-3], bog.[Con-2], bog.[Con-1], bog.Con0, bog.Prd3, bog.Prd2, bog.Prd1, bog.Prd, bog.LeitOk, F.GrupoVenda, F.Email, E.EquiDesc, P.PdvLogradouroPV, P.PdvNumeroPV, P.PdvBairroPV, P.PdvComplementoPV, P.PdvCidadePV, P.PdvUfPV, P.PdvCEP, MAX(C.DtAberturaChamado) as UltChamado, C.Id FROM dbo.bogf_Leituras_QtdGrpT as bog inner join dbo.FilialEntidadeGrVenda as F on F.A1_GRPVEN = bog.GrpVen inner join dbo.Equipamento as E on E.EquiCod = bog.EquiCod left join dbo.PontoVenda as P on P.EquiCod = bog.EquiCod and P.PdvStatus = 'A' left join (select distinct EquiCod, MAX(Id) as Id, MAX(DtAberturaChamado) as DtAberturaChamado from dbo.ChamadosSL2MiFix where ChamadoFechado = 0 and ChamadoAberto = 1 group by EquiCod) as C on C.EquiCod = bog.EquiCod WHERE (((bog.GrpVen) = ?)) group by bog.GrpVen, bog.EquiCod, P.AnxDesc, bog.MáxDeDataLeitura, bog.[Ql-4], bog.[Ql-3], bog.[Ql-2], bog.[Ql-1], bog.Ql0, bog.[Con-4], bog.[Con-3], bog.[Con-2], bog.[Con-1], bog.Con0, bog.Prd3, bog.Prd2, bog.Prd1, bog.Prd, bog.LeitOk, F.GrupoVenda, F.Email, E.EquiDesc, P.PdvLogradouroPV, P.PdvNumeroPV, P.PdvBairroPV, P.PdvComplementoPV, P.PdvCidadePV, P.PdvUfPV, P.PdvCEP, C.Id order by EquiCod DESC"
+const queryTelemetrias =
+  "SELECT bog.GrpVen, bog.EquiCod, P.AnxDesc, bog.Prd3, bog.Prd2, bog.Prd1, bog.Prd, (ISNULL(bog.Prd3, 0) + ISNULL(bog.Prd2, 0) + ISNULL(bog.Prd1, 0) + ISNULL(bog.Prd, 0)) as Total FROM dbo.bogf_Leituras_QtdGrpT as bog left join dbo.PontoVenda as P on P.EquiCod = bog.EquiCod and P.PdvStatus = 'A' WHERE (((bog.GrpVen) = ?)) group by bog.GrpVen, bog.EquiCod, P.AnxDesc, bog.MáxDeDataLeitura, bog.[Ql-4], bog.[Ql-3], bog.[Ql-2], bog.[Ql-1], bog.Ql0, bog.[Con-4], bog.[Con-3], bog.[Con-2], bog.[Con-1], bog.Con0, bog.Prd3, bog.Prd2, bog.Prd1, bog.Prd, bog.LeitOk, P.PdvLogradouroPV, P.PdvNumeroPV, P.PdvBairroPV, P.PdvComplementoPV, P.PdvCidadePV, P.PdvUfPV, P.PdvCEP order by Total DESC";
+
+const queryEntregaDeOS =
+  "select OSCId, OSCExpDtPrevisao from SLAPLIC.dbo.OSCtrl where GrpVen = ? and OSCExpDtPrevisao is not null";
+
+const queryFaturamentoDosClientes =
+  "select C.Nome_Fantasia, A.AnxDiaFecha from SLAPLIC.dbo.Contrato as C inner join SLAPLIC.dbo.Anexos as A on C.ConId = A.ConId and A.CNPJ = C.CNPJ and C.GrpVen = A.GrpVen where A.GrpVen = ? and C.ConStatus = 'A'";
